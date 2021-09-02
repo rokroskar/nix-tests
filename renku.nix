762dd7b8bc9646c839c1e948b1cb0af38a494339
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2021-09-02 18:21:13 +0200

    mach-nix + renku attempt

--- renku.nix
+++ renku.nix
@@ -1,17 +1,16 @@
-{ pkgs ? import <nixpkgs> { }
-, pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
-}:
-
-with pkgs;
+{ system ? "x86_64-linux" }:
 let
+  pkgs = import <nixpkgs> { system = system; };
    mach-nix = import (builtins.fetchGit {
      url = "https://github.com/DavHau/mach-nix/";
-     ref = "refs/tags/2.2.2";
-   });
-
-in mach-nix.mkPython rec {
-  python = python39;
-  requirements =  ''
-    renku
-  '';
-}
+     ref = "refs/tags/3.3.0";
+   }) { inherit pkgs; };
+  renku = mach-nix.mkPython {
+    requirements =  ''
+      renku
+    '';
+    };
+in
+  mach-nix.nixpkgs.mkShell{
+    buildInputs = [ renku pkgs.git ];
+  }
